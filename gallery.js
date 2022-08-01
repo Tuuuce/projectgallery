0e62af94e52ba17dea88e29a13f40f012b59f9a4
Tuuuce <91470624+Tuuuce@users.noreply.github.com>
2022-08-01 11:42:20 +0100

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,16 +1,23 @@
-function MoreLess() {
-  var dots = document.getElementById("dots");
-  var moreText = document.getElementById("more");
-  var btnText = document.getElementById("myBtn");
+let noOfCharac = 150;
+let content = documen.querySelectorAll(".content");
+content.forEach(content => {
 
-  if (dots.style.display!="none") {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read Less"; 
-    moreText.style.display = "inline";
-  } else {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
+  if(content.textContent.length < noOfCharac){
+    content.nextElementSibling.style.display = 
+    "none";
   }
-}
+  else{
+    let displayText = content.textContent.slice
+    (0,noOfCharac);
+    let moreText = content.textContent.slice
+    (noOfCharac);
+    content.innerHTML `${displayText}<span class="dots"></span><span class="hide more"><${moreText}</span>`
+  }
+});
 
+function readMore(btn) {
+  let post = btn.parentElement;
+  post.querySelector(".dots").classList.toggle("hide");
+  post.querySelector(".more").classList.toggle("hide");
+  btn.textContent == "Read More" ? btn.textContent = "Read Less" : btn.textContent = "Read More";
+}
